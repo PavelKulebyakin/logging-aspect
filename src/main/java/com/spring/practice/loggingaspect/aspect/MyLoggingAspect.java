bca5437f95e2f394525d40be9919fb281ab28dc5
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-07-13 23:11:41 +0300

    Add logging aspect

---
+++ src/main/java/com/spring/practice/loggingaspect/aspect/MyLoggingAspect.java
@@ -0,0 +1,32 @@
+package com.spring.practice.loggingaspect.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyLoggingAspect.class);
+
+    @Around("execution(* com.spring.practice.loggingaspect.repository.*.*(..))")
+    public Object aroundAllRepositoryMethodAdvice
+            (ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println("Begin of " + methodName);
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+
+        System.out.println("End of " + methodName);
+
+        return targetMethodResult;
+    }
+}
---
+++ src/main/java/com/spring/practice/loggingaspect/repository/AbstractRepository.java
@@ -0,0 +1,4 @@
+package com.spring.practice.loggingaspect.repository;
+
+public interface AbstractRepository {
+}
