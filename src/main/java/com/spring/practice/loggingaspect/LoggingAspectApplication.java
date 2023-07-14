a0a642a0dc015c8c72eae871d491ae0cbc762483
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-07-14 13:24:30 +0300

    Initial commit

---
+++ src/main/java/com/spring/practice/loggingaspect/LoggingAspectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.practice.loggingaspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingAspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingAspectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/practice/loggingaspect/LoggingAspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.practice.loggingaspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingAspectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
